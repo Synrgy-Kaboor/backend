365f3f53f0328df2ceae189cfe57dc66d65c9cde
Alfrendo Silalahi <alfrendos72@gmail.com>
2023-12-26 21:40:25 +0700

    initial project

---
+++ src/main/java/com/synrgy/kaboor/backend/KaboorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.synrgy.kaboor.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaboorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaboorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/synrgy/kaboor/backend/KaboorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synrgy.kaboor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaboorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
