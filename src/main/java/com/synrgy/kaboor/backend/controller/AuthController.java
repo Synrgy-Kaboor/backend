e99bb548006da74144545206c3583c98ee9e9d61
Alfrendo Silalahi <alfrendos72@gmail.com>
2024-01-02 20:39:32 +0700

    init security

---
+++ src/main/java/com/synrgy/kaboor/backend/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.synrgy.kaboor.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/auth")
+public class AuthController {
+
+}
