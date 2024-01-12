c981eb743319a4cf084243031e4d608dcc0e8fc9
Alfrendo Silalahi <alfrendos72@gmail.com>
2024-01-12 16:12:36 +0700

    Example

--- src/main/java/com/synrgy/kaboor/backend/service/implementation/SimpleAuthService.java
+++ src/main/java/com/synrgy/kaboor/backend/service/implementation/SimpleAuthService.java
@@ -84,6 +84,7 @@ public class SimpleAuthService implements AuthService {
     @Override
     public OtpDtoResponse verifyOtp(OtpDtoRequest otpDtoRequest) {
         // TODO: Handle exceptions
+        //
 
         // Find OTP from User table
         User user = userRepository.findByOtp(otpDtoRequest.getOtp())
